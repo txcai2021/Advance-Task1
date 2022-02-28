89c8a7ef153ccf2679800b16ba6e218e65d5509a
TR-RPS-Admin <tr-rps-admin@gmail.com>
2022-02-28 14:16:04 +0800

    first commit

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using OrdersApi.Models;
+using RabbitMQ.Client;
+
+namespace OrdersApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly OrderDBContext _context;
+        private readonly IConfiguration env;
+
+        public OrdersController(OrderDBContext context, IConfiguration env)
+        {
+
+            _context = context;
+            this.env = env;
+        }
+
+        // GET: api/Orders
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Order>>> GetOrders()
+        {
+            return await _context.Orders.ToListAsync();
+        }
+
+        // GET: api/Orders/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Order>> GetOrder(int id)
+        {
+            var order = await _context.Orders.FindAsync(id);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return order;
+        }
+
+        // PUT: api/Orders/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOrder(int id, Order order)
+        {
+            if (id != order.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(order).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Orders
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Order>> PostOrder(Order order)
+        {
+            order.Status = "PENDING";
+            _context.Orders.Add(order);
+            await _context.SaveChangesAsync();
+
+            var factory = new ConnectionFactory() { 
+                HostName = env.GetSection("RABBITMQHOST").Value, 
+                Port = Convert.ToInt32(env.GetSection("RABBITMQPORT").Value), 
+                UserName = env.GetSection("RABBITUSER").Value, 
+                Password = env.GetSection("RABBITPASSWORD").Value 
+            };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "orders", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                var message = Newtonsoft.Json.JsonConvert.SerializeObject(order);               
+                //string message = "Type: ORDER_CREATED | Order ID:" + order.Id + "|Order Price:" + order.Total + "|Order Details:" + order.Details + "|Customer Id:" + order.CustomerId;
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "", routingKey: "orders", basicProperties: null, body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+            return CreatedAtAction("GetOrder", new { id = order.Id }, order);
+        }
+
+        // DELETE: api/Orders/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrder(int id)
+        {
+            var order = await _context.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            _context.Orders.Remove(order);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool OrderExists(int id)
+        {
+            return _context.Orders.Any(e => e.Id == id);
+        }
+    }
+}
